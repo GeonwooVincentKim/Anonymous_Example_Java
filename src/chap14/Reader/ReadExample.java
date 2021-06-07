2de0b0e90ca24b8e5e9f140f3b4615ecaad93842
김건우 <kw.kim@xenoimpact.com>
2021-06-07 20:34:50 +0900

    [Feature] - Read file

--- src/chap14/Reader/ReadExample.java
+++ src/chap14/Reader/ReadExample.java
@@ -10,5 +10,20 @@ public class ReadExample {
         String fileLocate = "test8.txt";
         Reader reader = new FileReader("E:/Java_HonGong/JavaClassBasicStudy/NestedClass/src/chap14/" + fileLocate);
         out.println(reader);
+
+        char[] buffer = new char[100];
+        out.println(buffer);
+
+        while (true) {
+            int readCharNum = reader.read(buffer);
+            if (readCharNum == -1)
+                break;
+
+            for (int i = 0; i < readCharNum; i++) {
+                out.println(buffer[i]);
+            }
+        }
+
+        reader.close();
     }
 }
