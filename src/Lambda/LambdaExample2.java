d7b4936d52350c9cab2a01787941a0223b575055
GeonwooVincentKim <kdsnop@gmail.com>
2021-08-01 21:11:06 +0900

    [Feature] - Updated Lambda Example.java file

--- src/Lambda/LambdaExample2.java
+++ src/Lambda/LambdaExample2.java
@@ -10,10 +10,29 @@ public class LambdaExample2 {
         };
 
         lambdaExample.method();
+
+        out.println("-----------------------");
+        Outer o = new Outer();
+        out.println(o);
+        o.method();
     }
 }
 
 @FunctionalInterface
 interface LambdaInterface {
     void method();
+}
+
+class Outer {
+    public int iv = 10;
+
+    void method() {
+        final int iv = 40;
+        LambdaInterface f5 = () -> {
+            out.println("Outer.this.iv : " + Outer.this.iv);
+            out.println("this.iv : " + this.iv);
+            out.println("iv : " + iv);
+        };
+        f5.method();
+    }
 }
